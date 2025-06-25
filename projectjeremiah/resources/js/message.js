b388ac4d430953e25f7071e977f260b0d8afe3d7
Franchielle <palenciafa@students.nu-laguna.edu.ph>
2025-06-25 20:22:55 +0800

    Remove unused views, routes, and configuration files related to user services and authentication. This includes the deletion of counselor details, services, and welcome views, as well as associated routes and test files. Clean up storage directory by removing unnecessary .gitignore files and configuration files for Tailwind and Vite.

--- projectjeremiah/resources/js/message.js
+++
@@ -1,9 +0,0 @@
-function hideMessage() {
-    if (document.querySelector(".floating-window")) {
-        document.querySelector(".floating-window").classList.add('closed');
-    }
-}
-
-window.addEventListener("DOMContentLoaded", function() {
-        setTimeout(hideMessage, 1500);
-    }, false);
